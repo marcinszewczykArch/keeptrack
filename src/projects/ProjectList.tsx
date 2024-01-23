abbd53384c790967e505bad48097045728b5c5eb
marcin.szewczyk <marcin.szewczyk@ocado.com>
2024-01-23 14:05:58 +0100

    initial commit

---
+++ src/projects/ProjectList.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {Project} from './Project';
+import ProjectCard from "./ProjectCard";
+
+interface ProjectListProps {
+    projects: Project[];
+}
+
+function ProjectList({projects}: ProjectListProps) {
+    return (
+        <div className="row">
+
+            {projects.map((project) => (
+                <div key={project.id} className="cols-sm">
+                    <ProjectCard project={project}></ProjectCard>
+                </div>
+            ))}
+
+        </div>
+    );
+}
+
+export default ProjectList;
