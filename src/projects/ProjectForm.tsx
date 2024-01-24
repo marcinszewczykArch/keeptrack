df076c839f66793becde624798e14180d6bdbd0a
marcin.szewczyk <marcin.szewczyk@ocado.com>
2024-01-24 15:28:38 +0100

    add validation to form

--- src/projects/ProjectForm.tsx
+++ src/projects/ProjectForm.tsx
@@ -14,36 +14,58 @@ function ProjectForm({
                      }: ProjectFormProps) {
 
     const [project, setProject] = useState(initialProject);
+    const [errors, setErrors] = useState({
+        name: '',
+        description: '',
+        budget: '',
+    });
     const handleSubmit = (event: SyntheticEvent) => {
         event.preventDefault();
-        onSave(project);
+        return isValid() ? onSave(project) : {}
     };
-
     const handleChange = (event: any) => {
         const {type, name, value, checked} = event.target;
-        // if input type is checkbox use checked
-        // otherwise it's type is text, number etc. so use value
         let updatedValue = type === 'checkbox' ? checked : value;
-
-        //if input type is number convert the updatedValue string to a +number
         if (type === 'number') {
             updatedValue = Number(updatedValue);
         }
         const change = {
             [name]: updatedValue,
         };
-
         let updatedProject: Project;
-        // need to do functional update b/c
-        // the new project state is based on the previous project state
-        // so we can keep the project properties that aren't being edited +like project.id
-        // the spread operator (...) is used to
-        // spread the previous project properties and the new change
         setProject((project) => {
             console.log('change ', change);
-            return new Project({...project, ...change});
+            updatedProject = new Project({...project, ...change})
+            return updatedProject;
         });
+
+        setErrors(() => validate(updatedProject))
     };
+
+    function validate(project: Project) {
+        let errors: any = {name: '', description: '', budget: ''};
+        if (project.name.length === 0) {
+            errors.name = 'Name is required';
+        }
+        if (project.name.length > 0 && project.name.length < 3) {
+            errors.name = 'Name needs to be at least 3 characters.';
+        }
+        if (project.description.length === 0) {
+            errors.description = 'Description is required.';
+        }
+        if (project.budget <= 0) {
+            errors.budget = 'Budget must be more than 0.';
+        }
+        return errors;
+    }
+
+    function isValid() {
+        return (
+            errors.name.length === 0 &&
+            errors.description.length === 0 &&
+            errors.budget.length === 0
+        );
+    }
 
     return (
         <form className="input-group vertical"
@@ -57,6 +79,11 @@ function ProjectForm({
                 value={project.name}
                 onChange={handleChange}
             />
+            {errors.name.length > 0 && (
+                <div className="card error">
+                    <p>{errors.name}</p>
+                </div>
+            )}
             <label htmlFor="description">Project Description</label>
             <textarea
                 name="description"
@@ -64,6 +91,11 @@ function ProjectForm({
                 value={project.description}
                 onChange={handleChange}
             />
+            {errors.description.length > 0 && (
+                <div className="card error">
+                    <p>{errors.description}</p>
+                </div>
+            )}
             <label htmlFor="budget">Project Budget</label>
             <input
                 type="number"
@@ -72,6 +104,11 @@ function ProjectForm({
                 value={project.budget}
                 onChange={handleChange}
             />
+            {errors.budget.length > 0 && (
+                <div className="card error">
+                    <p>{errors.budget}</p>
+                </div>
+            )}
             <label htmlFor="isActive">Active?</label>
             <input
                 type="checkbox"
